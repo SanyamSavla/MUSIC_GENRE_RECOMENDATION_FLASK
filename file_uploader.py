c26f07eb97c3bdf40fa2841b7800622dda48394d
SanyamSavla <61912569+SanyamSavla@users.noreply.github.com>
2021-04-09 13:47:37 +0530

    musicplayer, ui fix, mongodb store

--- file_uploader.py
+++ file_uploader.py
@@ -4,13 +4,27 @@ import pickle
 import librosa
 import numpy as np
 import bcrypt
+import os
 from sklearn.preprocessing import MinMaxScaler
 from os import path
 from pydub import AudioSegment
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_pymongo import PyMongo
 import re
+import json
+from gridfs import GridFS
+from pymongo import MongoClient
+import sys
+import logging 
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
 
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
 
 app = Flask(__name__,template_folder='template')
 
@@ -22,7 +36,60 @@ scaler = pickle.load(open('scaler.pkl','rb'))
 app.config['MONGO_DBNAME'] = 'mip'
 app.config['MONGO_URI'] = 'mongodb+srv://sanyam:sanyam@cluster0.s7zmx.mongodb.net/test'
 mongo = PyMongo(app)
+mongo_client = MongoClient('mongodb+srv://sanyam:sanyam@cluster0.s7zmx.mongodb.net/test')
+db = mongo_client['mip']
+grid_fs = GridFS(db)
 
+def return_dict():
+    #Dictionary to store music file information
+    files=db.fs.files
+    result=files.find_one(sort=[( '_id', -1 )])
+    dict_here = [
+        {'id': 1, 'name': 'test', 'link': 'hiphop.00006.wav', 'genre': 'hip'},
+        
+       ]
+    return dict_here
+
+#Route to render GUI
+@app.route('/play')
+def show_entries():
+    general_Data = {
+        'title': 'Music Player'}
+        
+    files=db.fs.files
+    result=files.find_one(sort=[( '_id', -1 )])
+    
+    stream_entries = result
+    print(stream_entries)
+      
+    return render_template('play.html', entries=stream_entries, **general_Data)
+
+#Route to stream music
+@app.route('/int:<stream_id>')
+def streammp3(stream_id):
+    def generate():
+        data = return_dict()
+        count = 1
+        for item in data:
+            if item['id'] == stream_id:
+                song = item['link']
+        with open(song, "rb") as fwav:
+            data = fwav.read(1024)
+            while data:
+                yield data
+                data = fwav.read(1024)
+                logging.debug('Music data fragment : ' + str(count))
+                count += 1
+                
+    return Response(generate(), mimetype="audio/wav")
+
+
+@app.route('/home1')
+def test():
+    if 'name' in session:
+        text='Loged in'
+        return render_template('test.html',text=text)
+    return render_template('test.html')
 #login
 @app.route('/home')
 def home():
@@ -32,7 +99,7 @@ def home():
 def index():
     if 'name' in session:
         message='You are logged in as ' + session['name']
-        return render_template('home.html',message=message) 
+        return render_template('test.html',message=message) 
 
     return render_template('signin.html')
 
@@ -55,6 +122,7 @@ def login():
     error='Invalid username/password combination'
     return render_template('signin.html',error=error)  
 
+
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
@@ -65,12 +133,14 @@ def register():
             hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
             users.insert({'name' : request.form['name'], 'password' : (hashpass)})
             session['name'] = request.form['name']
-            return redirect(url_for('index'))
+            return redirect(url_for('test'))
+
         
         return 'That username already exists!'
 
     return render_template('index.html')
 
+   
 def getmetadata(filename):
     y, sr = librosa.load(filename)
     onset_env = librosa.onset.onset_strength(y, sr)
@@ -122,15 +192,27 @@ def getmetadata(filename):
 def upload():
     return render_template("file_upload_form.html")
 
+
+
 @app.route('/success', methods = ['POST'])
 def success():
+    UPLOAD_FOLDER = '/uploads'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     if request.method == 'POST':
         f = request.files['file']
+        #os.path.join('C:/Users/DELL/Desktop/projects/MIP_MusicPrediction_final/MUSIC_GENRE_RECOMENDATION_FLASK/uploads/',
         f.save(f.filename)
         f_name = f.filename.split('.')
 
+       
+
         if(f_name[-1] != 'wav'):
-            file_name = 'Upload only wav file'
+            dst="file.wav"
+            print(f.filename)
+            sound = AudioSegment.from_mp3(f.filename)
+            
+            sound.export(dst, format="wav")
+            file_name = dst
         else:
             file_name = f.filename
         genre = {0: 'blues', 1: 'classical', 2: 'country', 3: 'disco', 4: 'hiphop', 5: 'jazz', 6: 'metal', 7: 'pop', 8: 'reggae',9: 'rock'}
@@ -141,8 +223,14 @@ def success():
         mtdt_scaled = scaler.transform([mtdt])
         pred_genre = model.predict(mtdt_scaled)
         genre_name = genre[pred_genre[0]]
-        return render_template("success.html", name = file_name, genre = genre_name)
-
+        print(session['name'])
+        
+        with grid_fs.new_file(filename=f.filename,name=session['name'],genre=genre_name,playid='1') as fp:
+            fp.write(request.data)
+            id=fp._id
+            
+        return render_template("success.html", name = file_name, genre = genre_name,entry=id)
+    
 if __name__ == '__main__':
     
     app.secret_key = 'mysecret'
